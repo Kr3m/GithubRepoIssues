4ee0b4284aa945a437fc0ed62050c54c5b304fe7
Kevin Remisoski <kremisoski@gmail.com>
2019-06-08 02:54:20 -0400

    First Commit

---
+++ assets/js/script.js
@@ -0,0 +1,111 @@
+$(document).ready(function () {
+    // $("#selSort").hide();
+    const octokit = new Octokit();
+    const fetchrepo = document.getElementById("fetchrepo");
+    const gitrepo = document.getElementById("gitrepo");
+    const info = document.getElementById("info");
+    const sortTable = document.getElementById("sortTable");
+    let arr = [];
+    let average_age = [];
+    let dates = [];
+    let open_issues = 0;
+    let length;
+    let owner;
+    let repo;
+    let htmlstr;
+
+    fetchrepo.addEventListener("submit", formHandler);
+    function formHandler(e) {
+        e.preventDefault();
+        let arr = gitrepo.value.split("/");
+        owner = arr[0];
+        repo = arr[1];
+        getData();
+    }
+
+    function getData() {
+        let req = octokit.issues.listForRepo({
+            owner: owner,
+            repo: repo
+        })
+        .then(({data}) => {
+            length = data.length;
+            for(let i = 0; i < length; i++) {
+                if(data[i].state === "open") {
+                    let obj = {};
+                    let m1 = moment();
+                    let m2 = moment(data[i].created_at);
+                    obj.age = m1.diff(m2, 'seconds');
+                    obj.created_at = data[i].created_at;
+                    obj.title = data[i].title;
+                    obj.comments = data[i].comments;
+                    obj.updated_at = data[i].updated_at;
+                    obj.labels = data[i].labels;
+                    arr.push(obj);
+                    open_issues = arr.length;
+                    dates.push(m2);
+                }
+            }
+
+            average_age = get_average_age(arr);
+            let maxDate = new Date(Math.max.apply(null, dates));
+            let minDate = new Date(Math.min.apply(null, dates));
+            htmlstr = "<h2>Info</h2><ul><li><strong>Open Issues: " + open_issues + "</strong></li><li><strong>Average Age: " + average_age +
+            "</strong></li><li><strong>Oldest Issue: " + minDate + "</strong></li><li><strong>Latest Issue: " + maxDate +
+            "</strong></li></ul>";
+            $(info).html(htmlstr);
+            $("#selSort").show();I made it
+
+            let oTable = $('#example').DataTable( {
+                paging: false,
+                searching: false,
+                info: false,
+                data: setData(arr),
+                columns: [
+                    {title: "Title"},
+                    {title: "Created On"},
+                    {title: "# of Comments"},
+                    {title: "Last Updated"},
+                    {title: "Labels"}
+                ]
+            });
+            sortTable.onchange = function () {
+                let val = parseInt(this.value);
+                oTable.order([val, 'asc']).draw();
+            };
+
+            function setData(arr) {
+                let dataArr = [];
+                for(let i =0; i < length; i++) {
+                    dataArr[i] = [arr[i].title, arr[i].created_at, arr[i].comments, arr[i].updated_at, getLabels(arr[i].labels)];
+                }
+                console.log(dataArr);
+                return dataArr;
+            }
+
+            function getLabels(lbl) {
+                if(lbl.length === 0) {
+                    return "No Labels";
+                } else {
+                    let str = "";
+                    lbl.forEach(function (e) {
+                        str += e.name + " ";
+                    });
+                    return str;
+                }
+            }
+        });
+        $(".dropdown a").click(function (e) {
+            e.preventDefault();
+            console.log(this.target.id);
+        });
+    }
+
+    function get_average_age($arr) {
+        let total = 0;
+        for(let i = 0; i < length; i++){
+            total += $arr[i].age;
+        }
+        return total / length;
+    }
+});
---
+++ index.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" href="assets/css/styles.css">
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col">
+				<form id="fetchrepo" action="#">
+					<div class="form-group">
+						<label for="gitrepo"><h2>Git Repo</h2></label>
+						<input class="form-control" type="text"id="gitrepo" name="gitrepo" aria-describedby="repoHelp">
+						<small id="repoHelp" class="form-text text-muted">Enter info like this: owner/repo and click Fetch</small>
+						<button type="submit" class="btn btn-primary">Fetch</button>
+					</div>
+				</form>
+			</div>
+			<div class="col">
+				<div id="info"></div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col">
+				<form id="selSort" action="#">
+					<div class="form-group">
+						<label for="sortTable"><h2>Sort Table</h2></label>
+						<select name="sortTable" id="sortTable" class="form-control">
+							<option value=""></option>
+							<option value="1">Created Date</option>
+							<option value="2"># of Comments</option>
+							<option value="3">Last Updated</option>
+						</select>
+					</div>
+				</form>
+			</div>
+			<div class="col"></div>
+		</div>
+		<div class="row">
+			<div class="col">
+				<table class="table" id="example"></table>
+			</div>
+		</div>
+	</div>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script src="assets/js/octokit-rest.min.js"></script>
+<!--<script src="https://github.com/octokit/rest.js/releases/download/v16.28.0/octokit-rest.js"></script>-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+<script src="assets/js/script.js"></script>
+</body>
+</html>
